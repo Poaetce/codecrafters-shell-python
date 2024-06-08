662bcbbf02985e21b74eabf6cca216b9afbd5e33
Poa <poaetcethe2nd@gmail.com>
2024-06-09 03:24:50 +0900

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 import sys
 
 
-def main():
-    sys.stdout.write("$ ")
+def main() -> None:
+    sys.stdout.write('')
     sys.stdout.flush()
 
-    input()
+    user_input: str = input()
+    command: str = user_input.split(' ')[0]
+
+    match command:
+        case _:
+            sys.stdout.write(f"{command}: command not found")
+    
+    sys.stdout.write('\n')
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
